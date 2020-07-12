d20a1bcd4d4c2d78e4112e4ace22aa960dc0baf5
catza09 <iulian.petroiu@cigames.ro>
2020-07-12 14:27:03 +0300

    Second Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('flash');
+const passport = require('passport');
+const dotenv = require('dotenv');
+
+const container = require('./container');
+
+dotenv.config({ path: './config.env' });
+
+container.resolve(function (users) {
+  mongoose.Promise = global.Promise;
+  const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB connection successfull'));
+
+  const app = SetupExpress();
+  function SetupExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(3000, function () {
+      console.log('Listening on port 3000');
+    });
+    ConfigureExpress(app);
+
+    //setare router
+    const router = require('express-promise-router')();
+    users.SetRouting(router);
+    app.use(router);
+  }
+
+  function ConfigureExpress(app) {
+    require('./passport/passport-local');
+    app.use(express.static('public'));
+    app.use(cookieParser());
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(validator());
+    app.use(
+      session({
+        secret: 'thisisasecretkey',
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      })
+    );
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+  }
+});
