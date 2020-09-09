bd4368e4004b5d624fc44fd124ee4bcae1c170ee
catza09 <iulian.petroiu@cigames.ro>
2020-09-09 16:45:12 +0300

    Added Update

--- passport/passport-facebook.js
+++ passport/passport-facebook.js
@@ -22,7 +22,7 @@ passport.use(
       clientID: secret.facebook.clientID,
       clientSecret: secret.facebook.clientSecret,
       profileFields: ['email', 'displayName', 'photos'],
-      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      callbackURL: '/auth/facebook/callback',
       passReqToCallback: true,
     },
     (req, token, refreshToken, profile, done) => {
